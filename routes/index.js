204086b3f1fbcef7a017ea5c2094a25651639c86
jpaige343 <jenniferoneill129@gmail.com>
2021-02-12 11:15:03 -0600

    add about page

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const router = require("express").Router();
 router.get("/", function (req, res) {
   res.render("index", { title: "Home Page", user: req.user ? req.user : null });
 });
+router.get("/about", function (req, res) {
+  res.render("about", { title: "About Page", user: req.user ? req.user : null });
+});
 
 module.exports = router;
